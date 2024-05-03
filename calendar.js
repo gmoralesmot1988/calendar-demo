9f60b501bd9b9622cfafe6941ccfcfd40d090081
gmoralesmot1988 <122378826+gmoralesmot1988@users.noreply.github.com>
2024-05-03 09:56:37 +0200

    Update calendar.js

--- calendar.js
+++ calendar.js
@@ -1,4 +1,4 @@
-<script src='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.10/index.global.min.js'></script>
+
 <script>
 function convertDateToISO(dateString) {
     var parts = dateString.split(' ');
